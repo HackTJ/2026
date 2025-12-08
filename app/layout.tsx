1dd9e2370d4ad60b678c038de55d53f2a43a63f4
ssbdragonfly <ssbdragonfly@gmail.com>
2025-12-07 23:04:58 -0500

    metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,35 @@ const dosis = Dosis({
   subsets: ["latin"],
 });
 
+const title = "HackTJ 13.0";
+const description =
+  "Do you get excited watching your ideas come to life? Ever wanted to build the next Snapchat or create an Amazon Alexa skill? Program a drone? Create programs using the latest machine learning or virtual reality technology? Come to HackTJ to make new friends, build mind-blowing projects, attend workshops to learn new tricks, and have a fantastic time!";
+const ogImage = "/pink_black.png";
+
 export const metadata: Metadata = {
-  title: "HackTJ 13.0",
-  description:
-    "Do you get excited watching your ideas come to life? Ever wanted to build the next Snapchat or create an Amazon Alexa skill? Program a drone? Create programs using the latest machine learning or virtual reality technology? Come to HackTJ to make new friends, build mind-blowing projects, attend workshops to learn new tricks, and have a fantastic time!",
+  title,
+  description,
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+  },
+  openGraph: {
+    title,
+    description,
+    type: "website",
+    images: [
+      {
+        url: ogImage,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+    images: [ogImage],
+  },
 };
 
 export default function RootLayout({
