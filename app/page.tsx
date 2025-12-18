b1514934324e9ee29f45cf6638ef9d1758a80b97
Shreyas Jain <shrys.jain@gmail.com>
2025-12-18 17:35:17 -0500

    add snow

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
 import PhotoStrip from "@/components/photo-strip";
@@ -6,10 +8,14 @@ import Signpost from "@/components/signpost";
 import Sponsors from "@/components/sponsors";
 import Location from "@/components/location";
 import Footer from "@/components/footer";
+import Snowfall from "react-snowfall";
 
 export default function Home() {
   return (
     <div>
+      <div className="fixed inset-0 pointer-events-none z-50">
+        <Snowfall />
+      </div>
       <Navbar />
       <Hero />
       <PhotoStrip />
